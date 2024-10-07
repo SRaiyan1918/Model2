f932385801493231917a08c26c1102ae68c336d0
Md Shagaf Raiyan Rashid <148724219+SRaiyan1918@users.noreply.github.com>
2024-10-07 19:56:16 +0530

    app.py

--- app.py
+++ app.py
@@ -1,12 +1,12 @@
 from flask import Flask,render_template,request,redirect
 from flask_cors import CORS,cross_origin
-import pickle
+import joblib
 import pandas as pd
 import numpy as np
 
 app=Flask(__name__)
 cors=CORS(app)
-model=pickle.load(open('LinearRegressionModel.pkl','rb'))
+model=joblib.load(open('LinearRegressionModel.pkl','rb'))
 car=pd.read_csv('Cleaned_Car_data.csv')
 
 @app.route('/',methods=['GET','POST'])
